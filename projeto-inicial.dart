f76139b6b821fd05829b75ac4c7679b261725fbd
Karen Nakahara <karenakahara@gmail.com>
2023-04-17 22:28:36 -0300

    variables

---
+++ projeto-inicial.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  int idade = 29;
+  double altura = 1.54;
+  bool isDev = true;
+  String nome = 'Karen Nakahara';
+  String apelido = 'Naka';
+  String frase = 'Meu nome é $nome \n'
+      'mas meu apelido é $apelido \n'
+      'sou desenvolvedora? $isDev \n'
+      'tenho $idade anos de idade e \n'
+      '$altura metros de altura';
+
+  print(frase);
+}
